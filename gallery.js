a56d00df4394938b9068f47347a4a2bde5994928
Jimmy D <jd9192016@gmail.com>
2018-11-23 01:25:01 -0500

    last peer review

--- gallery.js
+++ gallery.js
@@ -1,2 +1,25 @@
-const searchBar=document.forms['search-box'];
-console.log(searchBar.innerHTML);
+
+// Select teh input field
+const searchBar = document.forms["search-box"].querySelector("#search");
+const gallery = document.querySelector(".gallery");
+// add even listener to the input field
+
+searchBar.addEventListener('keyup', function(event){
+  // add the input value to a variable
+  const inputs = event.target.value.toLowerCase();
+  const pics = gallery.getElementsByTagName("a");
+  Array.from(pics).forEach((pic) =>{
+     const cap= pic.dataset.title;
+     if(cap.toLowerCase().indexOf(event.target.value)!= -1){
+       pic.style.display="block";
+     }else{
+       pic.style.display="none";
+     }
+  });
+
+});
+
+searchBar.addEventListener("blur", function(event){
+  event.target.value="";
+
+});
